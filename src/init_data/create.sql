9dba609fcae23083fc0822dbc5fceae66bbf3356
Toasted-Rice <javo9205@colorado.edu>
2021-10-30 11:14:42 -0600

    db table updates;

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -1,22 +1,32 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS documents;
+CREATE TABLE users(
+	id BIGSERIAL PRIMARY KEY NOT NULL,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	password VARCHAR(50) NOT NULL,
+	created_on TIMESTAMP NOT NULL,
+	last_login TIMESTAMP
+);
 
-CREATE TABLE users
-(id BIGSERIAL PRIMARY KEY NOT NULL,
-username VARCHAR(200) UNIQUE NOT NULL,
-password VARCHAR(200) NOT NULL);
+CREATE TABLE documents(
+	user_id BIGINT NOT NULL,
+	title VARCHAR(100) NOT NULL,
+	folder VARCHAR (50) NOT NULL,
+	delta VARCHAR(10485760) NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users (id),
+	CONSTRAINT unique_file UNIQUE (user_id, title, folder)
+);
 
-CREATE TABLE documents
-(docOwner VARCHAR(200) NOT NULL,
-title VARCHAR(100) UNIQUE NOT NULL,
-folder VARCHAR (50) NOT NULL,
-deltaFile VARCHAR(10485760) NOT NULL);
+INSERT INTO users (username, password, created_on)
+VALUES ('admin', 'password', NOW()), ('lackey', 'password', NOW());
 
-INSERT INTO documents (docowner, title, folder, deltafile)
-VALUES 
-       ('james3', 'same document', 'jamesfolder', '{{"ops": [{"insert": "Gandalf the Grey\n"}]}'),
-       ('james3', 'yuhyuh', 'jamesfolder', '{{"ops": [{"insert": "Gandalf the Grey\n"}]}'),
-       ('james3', 'youknowthedrill', 'jamesfolder', '{{"ops": [{"insert": "Gandalf the Grey\n"}]}'),
-       ('james3', 'alldeezfiles', 'jamesfolder', '{{"ops": [{"insert": "Gandalf the Grey\n"}]}'),
-       ('james3', 'anotha one', 'jamesfolder', '{{"ops": [{"insert": "Gandalf the Grey\n"}]}');
+INSERT INTO documents (user_id, title, folder, delta)
+SELECT id, title, folder, delta FROM users
+RIGHT JOIN (VALUES
+	('admin', 'same document',		'jamesfolder', '{{"ops": [{"insert": "Gandalf the Grey\n"}]}}'),
+	('admin', 'yuhyuh',				'jamesfolder', '{{"ops": [{"insert": "Gandalf the Grey\n"}]}}'),
+	('admin', 'youknowthedrill',	'jamesfolder', '{{"ops": [{"insert": "Gandalf the Grey\n"}]}}'),
+	('admin', 'alldeezfiles',		'jamesfolder', '{{"ops": [{"insert": "Gandalf the Grey\n"}]}}'),
+	('admin', 'anotha one',			'jamesfolder', '{{"ops": [{"insert": "Gandalf the Grey\n"}]}}'),
+	('lackey', 'anotha one',		'jamesfolder', '{{"ops": [{"insert": "Gandalf the Grey\n"}]}}')
+) AS doc (owner, title, folder, delta)
+ON owner = username;
 
