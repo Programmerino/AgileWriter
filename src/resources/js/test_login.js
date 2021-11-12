bf0a4b5e95b599f5b97374e40a5e0fe8b62c6689
Toasted-Rice <javo9205@colorado.edu>
2021-11-12 15:35:54 -0700

    Setting up to push to heroku

--- src/resources/js/test_login.js
+++
@@ -1,6 +0,0 @@
-function loginAsTestUser()
-{
-	document.getElementById("username").value = "test";
-	document.getElementById("userPassword").value = "password";
-	document.getElementById("login-button").click();
-}
