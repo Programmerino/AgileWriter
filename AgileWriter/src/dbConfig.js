e86893fe9e79dd0700ac611e2a70efaec5473e89
Toasted-Rice <javo9205@colorado.edu>
2021-11-12 20:47:13 -0700

    Project now pushes successfully to heroku

--- AgileWriter/src/dbConfig.js
+++ AgileWriter/src/dbConfig.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const { Pool } = require("pg");
 const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
-const postgres = new Pool({ connectionString: isProduction ? process.env.DATABASE_URL : connectionString });
+const postgres = new Pool({
+	connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+	ssl: { rejectUnauthorized: false }
+});
 postgres.connect();
 module.exports = { postgres };
