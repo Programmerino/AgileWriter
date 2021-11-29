bd02054dbbf69111232ddb90543d68b6f5ac7ca3
Toasted-Rice <javo9205@colorado.edu>
2021-11-29 14:28:36 -0700

    Pre-final submission

--- AgileWriter/src/dbConfig.js
+++ AgileWriter/src/dbConfig.js
@@ -5,7 +5,7 @@ const { Pool } = require("pg");
 const { parse } = require("pg-connection-string");
 const connectionString = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`;
 const config = parse(isProduction ? process.env.DATABASE_URL : connectionString);
-if (isProduction) config.ssl = {rejectUnauthorized: false};
+// if (isProduction) config.ssl = {rejectUnauthorized: false};
 const postgres = new Pool(config);
 postgres.connect();
 module.exports = postgres;
